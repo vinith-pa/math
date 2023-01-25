d3de9d79ae6cd6ae19292ca15697917367be99fd
Vinith P <vinithpach@gmail.com>
2023-01-25 23:45:12 +0530

    Random number display Checks each number Refreshes the page with refresh button Takes input with keyboard

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
 "use strict";
+const random = Math.floor(Math.random() * 90);
+const active = document.getElementsByClassName("cursor");
+const guess = document.getElementsByClassName("guess");
+const answer = (random + 9).toString();
+let rem = answer;
 
-window.addEventListener("keydown", check);
-
-const firstDigit = document.getElementById("firstDigit");
-const secondDigit = document.getElementById("secondDigit");
-
-const checkNumber = (input) => {
-  if (Number(input)) {
-    return Number(input);
-  }
-};
+document.getElementById("number").innerText = random;
 
 function check(input) {
-  if (checkNumber(input.key) == 7) {
-    secondDigit.innerText = input.key;
-    secondDigit.style.background = "green";
-    secondDigit.classList.toggle("cursor");
-    firstDigit.classList.toggle("cursor");
+  if (rem.endsWith(input)) {
+    active[0].innerText = input;
+    rem = answer.slice(0, -1);
+    active[0].style.background = "green";
+    active[0].classList.toggle("cursor");
+    guess[0].classList.toggle("cursor");
+  } else {
+    active[0].style.background = "red";
+    active[0].innerHTML = input;
   }
 }
+
+const numberButtons = document.querySelectorAll("[data-number]");
+const numberDelete = document.querySelector("[data-delete]");
+
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    console.log(rem);
+    check(button.innerText);
+  });
+});
+
+numberDelete.addEventListener("click", () => {
+  location.reload();
+});
